adafc487018fceedbee503f5a318093252d78b1a
Gallon Zhou <gallonzhou04@gmail.com>
2024-04-04 17:05:03 +1300

    feat: Added Meshing Logic - Defined vertex and index data - Using instancing for performance boost

--- src/world/chunk.py
+++ src/world/chunk.py
@@ -1,20 +1,72 @@
-from src.math.math import Math
+import numpy
+import OpenGL.GL as gl
 
-from src.constants.world_constants import CHUNK_SIZE
+from src.constants.world_constants import VERTICES, INDICES
 
 
 class Chunk:
-    def __init__(self, position):
-        self.position = position
-        self.blocks = numpy.zeros(3 * [CHUNK_SIZE])
+    def __init__(self, x, y, z):
+        self.vao = None
+        self.positions = []
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.blocks = numpy.zeros(3 * [1], dtype=numpy.uint8)
+        self.block_positions = self.get_block_positions()
+        self.set_buffers(self.block_positions)
 
     def create_chunk(self):
         for x in range(1):
             for y in range(1):
                 for z in range(1):
-                    self.add_block(Math.Vector3(x, y, z))
+                    self.add_block(x, y, z)
 
-    def add_block(self, position):
-        self.blocks.append()
+    def add_block(self, x, y, z, block_type=0):
+        self.blocks[x, y, z] = block_type
 
-    def render(self): ...
+    def get_block_positions(self):
+        block_positions = []
+
+        for x in range(1):
+            for y in range(1):
+                for z in range(1):
+                    block_positions.append((x, y, z))
+
+        return numpy.array(block_positions, dtype=numpy.float32)
+
+    def set_buffers(self, block_positions):
+        vertices = numpy.array(VERTICES, dtype=numpy.float32)
+        indices = numpy.array(INDICES, dtype=numpy.uint32)
+
+        self.vao = gl.glGenVertexArrays(1)
+        gl.glBindVertexArray(self.vao)
+
+        vbo = gl.glGenBuffers(1)
+
+        # Bind buffer
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices, gl.GL_STATIC_DRAW)
+        # Send buffer data to GPU
+        gl.glVertexAttribPointer(0, vertices.shape[1], gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+        gl.glEnableVertexAttribArray(0)
+
+        ebo = gl.glGenBuffers(1)
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ebo)
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, indices, gl.GL_STATIC_DRAW)
+
+        # uv at position 1, texture buffer at position 2, instancing at position 3
+        ibo = gl.glGenBuffers(1)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, ibo)
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, block_positions, gl.GL_STATIC_DRAW)
+        gl.glVertexAttribPointer(3, block_positions.shape[1], gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+        gl.glEnableVertexAttribArray(3)
+        gl.glVertexAttribDivisor(3, 1)
+
+    def render(self):
+
+        print(self.vao)
+        gl.glBindVertexArray(self.vao)
+        gl.glDrawElementsInstanced(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, None, len(self.block_positions))
+        gl.glBindVertexArray(0)
--- src/world/world.py
+++ src/world/world.py
@@ -1,25 +1,26 @@
 import numpy
 
-from math.Math import Math
-from world.chunk import Chunk
+from src.world.chunk import Chunk
 
 
 class World:
     def __init__(self):
-        self.chunks = []
+        self.chunks = numpy.empty(3 * [1], dtype=object)
 
     def create_world(self):
         for x in range(1):
             for y in range(1):
                 for z in range(1):
-                    self.add_chunk(Math.Vector3(x, y, z))
+                    self.add_chunk(x, y, z)
 
-    def add_chunk(self, position):
-        chunk = Chunk(position)
+    def add_chunk(self, x, y, z):
+        chunk = Chunk(x, y, z)
         chunk.create_chunk()
 
-        self.chunks.append(chunk)
+        self.chunks[x, y, z] = chunk
 
     def render(self):
-        for chunk in self.chunks:
-            chunk.render()
+        for x in range(1):
+            for y in range(1):
+                for z in range(1):
+                    self.chunks[x, y, z].render()
